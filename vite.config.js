8341160e3648a160bc22844df66da1f043e7aa35
prasanth9090 <prasanth.kunchanapalli007@gmail.com>
2025-07-13 21:05:22 -0400

    update vite.config.js for website

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/synexiai-website/', // 👈 use '/REPO-NAME/'
+  base: '/', // 👈 This is correct for custom domain!
   plugins: [react()],
 });
