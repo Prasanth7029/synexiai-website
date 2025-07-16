fe875b56a1f7ec83beb64173b5767ad6cbcf0e61
prasanth7029 <kvsprasanth007@gmail.com>
2025-07-16 11:41:53 -0400

    blog page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/HomePage.jsx";
-import AboutPage from "./pages/AboutPage.jsx";
-import ProjectsPage from "./pages/ProjectsPage.jsx";
-import BlogPage from "./pages/BlogPage.jsx";
-import VisionPage from "./pages/VisionPage.jsx";
-import BlogPostPage from "./pages/BlogPostPage.jsx";
-import NotFound from "./pages/NotFound";
-import ContactPage from "./pages/ContactPage";
-import TechStackPage from "./pages/TechStackPage";
-import ScrollToTop from "./components/ScrollToTop";
-import Layout from './components/Layout';
-
-
+import HomePage       from "./pages/HomePage.jsx";
+import AboutPage      from "./pages/AboutPage.jsx";
+import ProjectsPage   from "./pages/ProjectsPage.jsx";
+import BlogPage       from "./pages/BlogPage.jsx";
+import VisionPage     from "./pages/VisionPage.jsx";
+import BlogPostPage   from "./pages/BlogPostPage.jsx";
+import NotFound       from "./pages/NotFound";
+import ContactPage    from "./pages/ContactPage";
+import TechStackPage  from "./pages/TechStackPage";
+import ScrollToTop    from "./components/ScrollToTop";
+import Layout         from "./components/Layout";
 
 function App() {
   return (
     <Router>
-      <>
-        <ScrollToTop />
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/vision" element={<VisionPage />} />
-            <Route path="/blog/:slug" element={<BlogPostPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/tech" element={<TechStackPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </>
+      <ScrollToTop />
+      <Layout>
+        <Routes>
+          <Route path="/"               element={<HomePage />} />
+          <Route path="/about"          element={<AboutPage />} />
+          <Route path="/projects"       element={<ProjectsPage />} />
+          <Route path="/blog"           element={<BlogPage />} />
+          <Route path="/vision"         element={<VisionPage />} />
+          <Route path="/blog/:slug"     element={<BlogPostPage />} />
+          <Route path="/contact"        element={<ContactPage />} />
+          <Route path="/tech"           element={<TechStackPage />} />
+          <Route path="*"               element={<NotFound />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
